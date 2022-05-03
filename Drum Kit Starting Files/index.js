812fc375e4eb9ff3f18fe0593e49e26c07d707d9
Tilda16 <rebekkakruck@gmail.com>
2022-05-03 11:56:44 +0200

    added a finder for finding the html element that is clicked on.

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,7 +3,8 @@ let audio = new Audio('sounds/tom-1.mp3');
 
 for(i = 0; i<numberOfButtons; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-  function handleClick(){
-    audio.play();
-  }
+  console.log(this.innerHTML);
+  // function handleClick(){
+  //   audio.play();
+  // }
 }
