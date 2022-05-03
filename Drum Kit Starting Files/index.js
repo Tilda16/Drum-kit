743ba6a5859061527db2d3009c30ba11eff80709
Tilda16 <rebekkakruck@gmail.com>
2022-05-03 11:58:08 +0200

    Changing handle click to a function

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,9 +2,6 @@ let numberOfButtons = document.querySelectorAll(".drum").length;
 let audio = new Audio('sounds/tom-1.mp3');
 
 for(i = 0; i<numberOfButtons; i++){
-  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+  document.querySelectorAll(".drum")[i].addEventListener("click", function());
   console.log(this.innerHTML);
-  // function handleClick(){
-  //   audio.play();
-  // }
 }
