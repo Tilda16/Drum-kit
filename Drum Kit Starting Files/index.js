438e21c5855450f080b0f06e21590c7c4330194e
Tilda16 <rebekkakruck@gmail.com>
2022-05-04 11:03:32 +0200

    Changing color of text of clicked button

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,5 +3,5 @@ let audio = new Audio('sounds/tom-1.mp3');
 
 for(i = 0; i<numberOfButtons; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", function());
-  console.log(this.innerHTML);
+  this.style.color = "white";
 }
