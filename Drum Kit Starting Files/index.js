56fd55ed30725fae9d00e952a3d4533179eb84b9
Tilda16 <rebekkakruck@gmail.com>
2022-05-04 11:55:34 +0200

    Still trying to change the color of text when clicked

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,6 +2,6 @@ let numberOfButtons = document.querySelectorAll(".drum").length;
 let audio = new Audio('sounds/tom-1.mp3');
 
 for(i = 0; i<numberOfButtons; i++){
-  document.querySelectorAll(".drum")[i].addEventListener("click", function);
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
   this.style.color="white";
 }
