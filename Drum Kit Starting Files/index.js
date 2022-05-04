6573d966a9d02951460e10e61d1bf3106ad5e0e9
Tilda16 <rebekkakruck@gmail.com>
2022-05-04 11:59:47 +0200

    Got it!

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,6 +3,6 @@ let audio = new Audio('sounds/tom-1.mp3');
 
 for(i = 0; i<numberOfButtons; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-  this.style.color="white";
+  this.style.color = "white";
 });
 }
