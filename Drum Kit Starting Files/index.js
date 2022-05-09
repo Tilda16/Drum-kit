0ad8ca08af4bc617842312a1be1d242d3858f9f5
Tilda16 <rebekkakruck@gmail.com>
2022-05-09 13:00:16 +0200

    Added () to play in switch statement

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -7,31 +7,31 @@ for(i = 0; i<numberOfButtons; i++){
 switch (buttonInnerHTML) {
   case "w":
     let crash = new Audio("sounds/crash.mp3");
-    crash.play;
+    crash.play();
     break;
   case "a":
     let kickBase = new Audio("sounds/kick-base.mp3");
-    kickBase.play;
+    kickBase.play();
     break;
   case "s":
     let snare = new Audio("sounds/snare.mp3");
-    snare.play;
+    snare.play();
     break;
   case "d":
     let tom1 = new Audio("sounds/tom-1.mp3");
-    tom1.play;
+    tom1.play();
     break;
   case "j":
     let tom2 = new Audio("sounds/tom-2.mp3");
-    tom2.play;
+    tom2.play();
     break;
   case "k":
     let tom3 = new Audio("sounds/tom-3.mp3");
-    tom3.play;
+    tom3.play();
     break;
   case "l":
     let tom4 = new Audio("sounds/tom-4.mp3");
-    tom4.play;
+    tom4.play();
     break;
   default: console.log(buttonInnerHTML);
 
