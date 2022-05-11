acec4975379c4a508744414ea0a09670ba1f9044
Tilda16 <rebekkakruck@gmail.com>
2022-05-11 11:11:57 +0200

    Tidied up my javascript file a little

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,5 +1,4 @@
 let numberOfButtons = document.querySelectorAll(".drum").length;
-let audio = new Audio('sounds/tom-1.mp3');
 
 for(i = 0; i<numberOfButtons; i++){
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
